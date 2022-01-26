145b02228e6a3ab0ed8c7131e17df126dabce2cb
Victor Rentea <victorrentea@gmail.com>
2022-01-26 19:53:52 +0200

    fix startup

--- order/src/main/java/victor/training/microservices/order/OrderApplication.java
+++ order/src/main/java/victor/training/microservices/order/OrderApplication.java
@@ -1,28 +1,20 @@
 package victor.training.microservices.order;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import victor.training.microservices.message.DeliveryResponse;
-import victor.training.microservices.message.PaymentResponse;
-import victor.training.microservices.message.RestaurantResponse;
-import victor.training.microservices.order.SagaState.Status;
-import victor.training.microservices.order.context.SagaContext;
-
-import java.time.format.DateTimeFormatter;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-
-import static java.time.LocalDateTime.now;
+import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
 @Slf4j
 public class OrderApplication {
+
+   @Bean
+   public RestTemplate restTemplate() {
+      return new RestTemplate();
+   }
 
    public static void main(String[] args) {
       SpringApplication.run(OrderApplication.class, args);
